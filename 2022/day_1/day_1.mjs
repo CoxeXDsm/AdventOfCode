546ccd3ec07b461869d4b7f5e3bd9859fbd99bbb
coxexdsm <coxegiltorres@gmail.com>
2023-11-30 19:55:39 +0100

    build: add retos previos y preparar aoc 2023

--- 2022/day_1/day_1.mjs
+++ 2022/day_1/day_1.mjs
@@ -1,17 +1,37 @@
 import { readFileSync } from "node:fs";
 
-const instructions = readFileSync("2022\\day_1\\day_1.txt", { encoding: "utf-8" }) // read day_??.txt content
+const calories = readFileSync("2022\\day_1\\day_1.txt", { encoding: "utf-8" }) // read day_??.txt content
   .replace(/\r/g, "") // remove all \r characters to avoid issues on Windows
-  .trim(); // Remove starting/ending whitespace
+  .trim()
+  .split(/\n\n/g); // Remove starting/ending whitespace
 
 function part1(){
-    // add method
-    console.log('Solution 1: ');
+    let result = 0;
+    calories.forEach(element => {
+        let linea = element.replace(/\n/g, " ").split(" ");
+        let sum = linea.reduce((acc, value) => {return acc + Number(value)}, 0);
+        if(sum > result) result = sum;
+    });
+    console.log('Solution 1: ' + result);
 }
 
 function part2(){
-    // add method
-    console.log('Solution 2: ');
+    let result = [];
+    calories.forEach(element => {
+        let linea = element.replace(/\n/g, " ").split(" ");
+        let sum = linea.reduce((acc, value) => {return acc + Number(value)}, 0);
+        let minResult = Math.min.apply(null, result);
+        if(result.length < 3){
+            result.push(sum);
+        } else if(result.length == 3 && minResult < sum){
+            let index = result.indexOf(minResult);
+            result.splice(index, 1);
+            result.push(sum);
+        }
+    });
+
+    let solution = result.reduce((acc, value) => {return acc + Number(value)}, 0);
+    console.log('Solution 2: ' + solution);
 }
 
 // execute the parts
---
+++ 2023/day_1/day_1.mjs
@@ -0,0 +1,18 @@
+import { readFileSync } from "node:fs";
+
+const guide = readFileSync("2023\\day_1\\day_1.txt", { encoding: "utf-8" }) // read day_??.txt content
+  .replace(/\r/g, "") // remove all \r characters to avoid issues on Windows
+  .trim(); // Remove starting/ending whitespace
+
+function part1(){
+    // insert code
+    console.log('Solution 1: ');
+}
+
+function part2(){
+    // insert code
+    console.log('Solution 2: ');
+}
+
+// execute the parts
+part1(); part2();
